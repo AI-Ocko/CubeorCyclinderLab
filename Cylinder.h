70e6eae116058e5c16d9c2502e8c3bb0f0101c20
christopher <135375609+AI-Ocko@users.noreply.github.com>
2025-03-06 01:03:08 -0800

    Created Cylinder Class,	methods, getters, and setters

---
+++ Cylinder.h
@@ -0,0 +1,34 @@
+#ifndef _CYLINDER
+#define _CYLINDER
+
+class Cylinder {
+
+    public:
+        Cylinder();
+        Cylinder(double radiusPar, double heightPar);
+
+        void setHeight (double heightPar);
+        void setRadius (double radiusPar);
+
+        double getHeight();
+        double getRadius();
+        
+        double calcSurfaceArea(double radiusPar, double heightPar);
+        double calcVolume(double radiusPar, double heightPar);
+
+    private:
+        double radius;
+        double height;
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
